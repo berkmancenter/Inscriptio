7d7b6b9254e7178764697cb4e09f4d2c1ac09289
Daniel Collis-Puro <dan@collispuro.net>
2011-03-25 15:36:32 -0400

    Inscriptio with working cucumber tests.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inscriptio::Application.config.secret_token = 'e808974d6e57e38ed241e606a0214eda543cb5591b3d761df1c860c4bfdcd42d99c4339a2cadd15a7ef4a7f4ad7296d5090734f55d4747c3cfb43ae45120d40a'
