1d3987a311bcde32e63dfcf9df9faa1ae727ed90
djcp <dan@collispuro.net>
2011-10-31 14:56:34 -0400

    Full logging.

--- app/controllers/hauthproxy_controller.rb
+++ app/controllers/hauthproxy_controller.rb
@@ -1,8 +1,9 @@
 class HauthproxyController < ApplicationController
   def validate
+    logger.warn('Params: ' + params.inspect)
   end
 
   def invalid
+    logger.warn('Params: ' + params.inspect)
   end
-
 end
